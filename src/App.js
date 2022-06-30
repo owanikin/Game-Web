63ecb3831a8dd636cc2851771f72e3e312df4215
Owanikin <oderindeife@gmail.com>
2022-06-30 13:26:48 +0100

    web connect to wallet button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'owanikin';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // Just a state variable we use to store our user's public wallet. Don't forget to import useState.
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  // Actions that will run on component load. Since it will take some time, we should declare it as async
+  const checkIfWalletIsConnected = async () => {
+      try {
+        // Make sure we have access to window.ethereum
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+        return;
+        } else {
+          console.log("We have the ethereum object", ethereum);
+          
+          // Check if we're authorized to access the user's wallet.
+          const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+          // User can have multiple authorized accounts, we grab the first one if its there!
+          if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log('Found an authorized account:', account);
+            setCurrentAccount(account);
+          } else {
+            console.log('No authorized account found');
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }  
+  };
+
+  // Implementing your connectWallet method here
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+      // Request method access to account.
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      // This should print out public address once we authorize Metamask.
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // This runs our function when the page loads.
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <p className="header gradient-text">⚔️ Mortal Kombat ⚔️</p>
+          <p className="sub-text">Team up to protect the Mortals!</p>
           <div className="connect-wallet-container">
             <img
               src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
               alt="Monty Python Gif"
             />
+            <button className="cta-button connect-wallet-button" onClick={connectWalletAction}>
+              Connect Wallet To Get Started
+            </button>
           </div>
         </div>
         <div className="footer-container">
